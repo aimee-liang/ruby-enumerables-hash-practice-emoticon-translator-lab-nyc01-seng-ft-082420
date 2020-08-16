f78da087c35fe013ac6dbc28c062ce22ef126e5a
Aimee Liang <aimeeeliang@gmail.com>
2020-08-16 19:36:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,14 +27,4 @@ end
   
 
 def get_english_meaning(path, emoticon)
-  emoticons_library = load_library(path)
-
-  emoticons_library.select.do |key, value|
-    if emoticon == value[:japanese]
-      return key
-    end
-  end
-  unless emoticons_library.include?(emoticon)
-    return "Sorry, that emoticon was not found"
-  end
 end
