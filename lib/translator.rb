3ef57254604d38badb55efb9a2d434fc68118d73
Aimee Liang <aimeeeliang@gmail.com>
2020-08-16 19:56:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,7 +33,7 @@ def get_english_meaning(path, emoticon)
   emoticons_hash.each do |emoticon_name, result|
     result.each do |key, value|
       if emoticon == value
-        return result[key]
+        return result[:english]
         binding.pry
       end
     end
